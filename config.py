d10c12f40b733c5e3d43bb4024a98ac43a9c452f
Jeremy Tao <taoxianpeng@gmail.com>
2019-07-13 20:35:00 +0800

    环境变量配置文件

---
+++ config.py
@@ -0,0 +1,15 @@
+#自动设置环境变量
+#注意：设置的环境变量只是暂时的，当程序关闭的时候环境变量失效
+import os
+
+class setEnvVar():
+    def __init__(self):
+        ffmpeg_path = os.getcwd()+'\\ffmpeg\\bin'
+        if os.path.exists(ffmpeg_path):
+            try:
+                os.environ['PATH'] = ffmpeg_path
+                print('[INFO] set ffmpeg path ok')
+            except Exception as e :
+                print ('[INFO] '.join(e))
+        else:
+            print('当前目录下找不到ffmpeg文件，请检查！')
